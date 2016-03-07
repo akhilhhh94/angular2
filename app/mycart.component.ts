67106930f1732e2ad4319c2bf275780911bbb3bd
akhilhhh94 <akhil.h@bridge-india.in>
2016-03-07 12:56:24 +0530

    cart clearing

--- app/mycart.component.ts
+++ app/mycart.component.ts
@@ -10,26 +10,39 @@ export class MyCart {
 
 
 	addToCart(item: Product) {
-
 		this.cart.push(item);
 		this.updateDom();
-
 	}
+
 	getCart() {
 		return this.cart;
 	}
+
 	clearCart(){
-		return this.cart = [];
+		this.cart = [];
+		this.updateDom();
+		return true;
 	}
-	removeItem(item:Product){
-		this.cart.filter(it=> it.id == item.id);
+
+	removeItem(item: Product) {
+		var tempCart = [];
+		this.cart.forEach(itm=> {
+			if (itm.id !== item.id) {
+				tempCart.push(itm);
+			}
+		});
+		this.cart = tempCart;
+		this.updateDom();
 	}
+
 	TotalItem(){
 		return this.cart.length || 0;
 	}
+
 	updateDom(){
         this.UpdateCart.emit(this.TotalItem());
 	}
+
 	getNavChangeEmitter() {
 	 	return this.UpdateCart;
 	}
--- app/showcart.component.ts
+++ app/showcart.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, EventEmitter } from 'angular2/core';
 import { MyCart } from './mycart.component';
 import { Product } from './product';
+
 @Component({
   selector: 'my-dashboard',
   template: `
     <h1>My Cart</h1>
     <div>
-    <table>
+    <table  *ngIf="CartItems>0">
       <tr>
         <td>Num</td>
         <td>Name</td>
@@ -15,11 +16,11 @@ import { Product } from './product';
  <tr *ngFor="#single of _myCartItems; #i = index" >
           <td>{{i+1}}</td>
         <td>{{single.name}}</td>
-        <td><button>x</button></td>
+        <td><button (click)="removeFromCart(single)">x</button></td>
         </tr> 
     </table>
     </div>
-
+    <div *ngIf="CartItems>0" (click)="ckeckout()"><button>checkout</button></div>
    `,
   styles: [`
     table{
@@ -28,11 +29,32 @@ import { Product } from './product';
   `]
 })
 
-export class ShowCart {
+export class ShowCart implements OnInit {
+
     public _myCartItems:Product[];
+
+     public CartItems:number=0;
     
     constructor(private _myCart:MyCart){
       this._myCartItems = _myCart.getCart();
+      this.CartItems = _myCart.TotalItem();
+   }
+
+    ngOnInit() { 
+      this._myCart.getNavChangeEmitter()
+        .subscribe(item =>  this.CartItems=item);
+    }
+
+    removeFromCart(item:Product){
+      this._myCart.removeItem(item);
+      this._myCartItems = this._myCart.getCart();
+      return true;  
+    }
+
+    ckeckout(){
+    this._myCart.clearCart();
+      this._myCartItems = this._myCart.getCart();
+      return true;
     }
 }
 
